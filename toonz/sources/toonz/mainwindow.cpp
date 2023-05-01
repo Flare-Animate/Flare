morevnaproject-org/opentoonz

#assistants: Add placeholder for icon of Assistant Tool

toonz/sources/toonz/mainwindow.cpp
@@ -2183,7 +2183,7 @@ void MainWindow::defineActions() {
   createToolAction(T_Plastic, "plastic", tr("Plastic Tool"), "X");
   createToolAction(T_Ruler, "ruler", tr("Ruler Tool"), "");
   createToolAction(T_Finger, "finger", tr("Finger Tool"), "");
-  createToolAction(T_EditAssistants, "geometric", tr("Edit Assistants"), "");
+  createToolAction(T_EditAssistants, "tool_assistant", tr("Assistant Tool"), "");
 
   createViewerAction(V_ZoomIn, tr("Zoom In"), "+");
   createViewerAction(V_ZoomOut, tr("Zoom Out"), "-");

Flare-Animate/Flare

toonz/sources/toonz/mainwindow.cpp
@@ -2472,7 +2472,7 @@ void MainWindow::defineActions() {
   createToolAction(T_Plastic, "plastic", QT_TR_NOOP("Plastic Tool"), "X");
   createToolAction(T_Ruler, "ruler", QT_TR_NOOP("Ruler Tool"), "");
   createToolAction(T_Finger, "finger", QT_TR_NOOP("Finger Tool"), "");
-  createToolAction(T_EditAssistants, "geometric", QT_TR_NOOP("Edit Assistants"), "");
+  createToolAction(T_EditAssistants, "tool_assistant", QT_TR_NOOP("Edit Assistants"), "");
 
   /*-- Animate tool + mode switching shortcuts --*/
   createAction(MI_EditNextMode, QT_TR_NOOP("Animate Tool - Next Mode"), "",